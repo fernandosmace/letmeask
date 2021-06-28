7c77c14fec961b7e18c826d3b0156ecaa92216ce
Fernando Macedo <fernando.soares.macedo@hotmail.com>
2021-06-27 22:24:32 -0300

    Criadas as páginas Index e NewRoom com roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from './Pages/Home';
+import { NewRoom } from './Pages/NewRoom';
+
 
 function App() {
   return (
-    <div>
-      <Button />
-    </div>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
   );
 }
 
